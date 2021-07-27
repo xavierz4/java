fc2196a5c88226cbd80fb2bab56401abd3876907
xavierz <xavierz@DESKTOP-UV655C7>
2021-07-26 21:27:57 -0500

    Hola Mundo+

---
+++ src/main/java/JavaEjercicios.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xavierz
+ */
+public class JavaEjercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(char c = 'a'; c <= 'z'; c++){
+            System.out.println(c);
+        }
+    }
+    
+}
