8cabf1ef8d9f2d4428d2e682dac9aab196e362c5
xavierz <xavierz@DESKTOP-UV655C7>
2021-07-26 21:37:02 -0500

    Hola Mundo+

---
+++ src/main/java/CicloFor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xavierz
+ */
+public class CicloFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0;
+        int j = 1;
+        
+        for(i = -13; i <= -10; i++){
+            j++;
+            
+        }
+        System.out.println(j);
+    }
+    
+}
