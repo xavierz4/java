3507331b25b9d55481b3a480c98beeed64b7deb7
xavierz <xavierz@DESKTOP-UV655C7>
2021-07-27 22:11:03 -0500

    Hola Mundo+

---
+++ src/main/java/ClasesObjetos.java
@@ -0,0 +1,82 @@
+/*
+public class Perro {
+    String nombre;
+    String raza;
+    Integer edad;
+}
+
+
+
+//
+public class Perro {
+    String nombre;
+    String raza;
+    int edad;
+    
+    void establecerAtributos(String nombre, String raza, int edad){
+       this.nombre= nombre;
+       this.raza=raza;
+       this.edad=edad;
+       
+    }
+Perro miparcero=new Perro();//instancia 
+        miparcero.establecerAtributos("miparcero", "Pastor belga", 2);
+       
+       
+        
+        
+        System.out.println("El nombre del perro es"+miparcero.nombre);  
+        System.out.println("La raza de mi perro es "+miparcero.raza);  
+        System.out.println("La edad de mi perro es "+miparcero.edad); 
+        
+        miparcero.ladrar();
+
+
+//
+public class Perro {
+// Atributos
+public int edad;
+public String nombre;
+public String colorOjos;
+// Constructor
+Perro(int edad, String nombre, String colorOjos) {
+this.edad = edad;
+this.nombre = nombre;
+this.colorOjos = colorOjos;
+}
+
+riangulo triangulo=new Triangulo();
+        triangulo.base=10;
+        triangulo.altura=20;
+        
+        float resultado=triangulo.area();
+        System.out.println("el resultado es"+resultado);
+public class Triangulo {
+    float base;
+    float altura;
+    
+    
+    float area(){
+      float area=(this.base*this.altura)/2;
+      return area;
+      
+    }
+    
+    
+}
+ */
+
+/**
+ *
+ * @author xavierz
+ */
+public class ClasesObjetos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
